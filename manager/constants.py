b4a95574ceb867146e97c79392f0905e548714de
taurus <andres.ch@protonmail.com>
2020-10-11 23:05:16 -0300

    feature: basic export method and check_init

--- manager/constants.py
+++ manager/constants.py
@@ -13,27 +13,39 @@ except KeyError:
 
 VERSION = "1.0.0"
 
-USAGE = """
+TITLE = """
          _
  ___ ___| |_ ._ _ _  ___ ._ _  ___  ___  ___  _ _
 <_-<<_-<| . || ' ' |<_> || ' |<_> |/ . |/ ._>| '_>
 /__//__/|_|_||_|_|_|<___||_|_|<___|\_. |\___.|_|
                                    <___'
-
 Easy way to remember your ssh conections.
+"""
 
+USAGE = """
 Usage:
     sshmanager init
-    sshmanager ( -c | --connect )
+    sshmanager (  c |   connect )
+    sshmanager (  n |   new )
+    sshmanager (  l |   list )
+    sshmanager (  r |   remove )
+    sshmanager (  u |   update )
+    sshmanager (  e |   export )
+    sshmanager (  i |   import )
     sshmanager ( -h | --help )
     sshmanager ( -v | --version )
-    sshmanager ( -e | --export )
 
 Commands:
     init                Initialize a SSHManager profile.
-    connect             Generate a new conection.
-    c                   Short way to connect command.
+    connect             Create a new ssh conection to the give id record.
+    new                 Save a new data conection.
+    remove              Remove a data conection.
+    update              Update a data conection.
+    list                List available conections.
+    export              Export the entired db.
+    import              Import the entired db from another sshmanager.
 
 Example:
-    sshmanager c
+    sshmanager connect 2
+    sshmanager export -p /home/ubuntu -e csv
 """
--- manager/manager.py
+++ manager/manager.py
@@ -7,24 +7,22 @@ import constants
 import pyinputplus as pyip
 import argparse
 from datetime import datetime
-from pprint import pprint
+from termcolor import colored, cprint
 from tabulate import tabulate
-
 from logger import logger
 
 def main():
     """Main function"""
     try:
         """Run user's input command."""
-
         # Initial log values
         logger.debug("###########################")
         logger.debug("### NEW PROCESS STARTED ###")
         logger.debug("###########################")
         logger.debug(sys.argv)
         logger.debug("USER: {0}".format(constants.USER))
-
         SSHManager()
+
     except KeyboardInterrupt:
         print("\nQuitting...")
         sys.exit(1)
@@ -46,34 +44,36 @@ class SSHManager(object):
         args = parser.parse_args(sys.argv[1:2])
 
         logger.debug("Main argument\n{0}".format(args))
+        logger.debug("All arguments\n{0}".format(sys.argv))
 
         if args.version:
-            print("SSHManager CLI v.{}".format(constants.VERSION))
+            cprint("SSHManager CLI v.{}".format(constants.VERSION), 'white')
             parser.exit(1)
         elif not args.command or not hasattr(self, args.command) or args.help:
-            print(constants.USAGE)
+            cprint(constants.TITLE, 'green')
+            cprint(constants.USAGE, 'white')
             parser.exit(1)
-
+        # To the respective command
+        cprint(constants.TITLE, 'green')
         getattr(self, args.command)()
 
 
     def init(self):
         if os.path.isfile('ssh-manager.db'):
 
-            print("A database is already configured.")
-            answer = pyip.inputChoice(['yes', 'no', 'y','n'], "Do you want remove it?(yes/no): ")
+            cprint("[!] A database is already configured.", 'yellow')
+            answer = pyip.inputChoice(['yes', 'no', 'y','n'], "Do you want remove it? (Yes/No): ")
 
-            if answer in ['y', 'yes']:
+            if answer in ['y', 'yes', 'Yes']:
                 # Remove old database and re-run this method
                 print("remove it")
                 os.remove('ssh-manager.db')
                 self.init()
             else:
-                # Exit from init
-                print("Dont remove!")
+                self.list()
 
         else:
-            print("Creating new database...")
+            cprint("[i] Creating new database.", 'yellow')
             conn = sqlite3.connect("ssh-manager.db")
             # The table structure for save configuration of conections
             conn.execute('''CREATE TABLE SSH(id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -85,19 +85,14 @@ class SSHManager(object):
                 pem_route TEXT,
                 created_at TEXT,
                 updated_at TEXT);''')
-            print("Database created.")
+            cprint("[i] Database created.", 'green')
             # ASk for save the first ssh conection
-            answer = pyip.inputChoice(['yes', 'no', 'y','n'], "Do you want to add your first ssh conection?(yes/no): ")
-            if answer in ['y', 'yes']:
-                #remove old database and re-run this method
-                self.new()
-            else:
-                # Exit from init
-                print('Init success, for start using add new conections')
+            self.ask_for_new_record()
             # closing the connection
             conn.close()
 
     def new(self):
+        self.check_init()
         print("Please enter the necesary data for save and create conections")
         user = pyip.inputStr("User name: ")
         host = pyip.inputStr("Host name: ")
@@ -125,16 +120,48 @@ class SSHManager(object):
         conn.commit()
         conn.close()
 
+    def check_init(self):
+        if os.path.isfile('ssh-manager.db'):
+            return True
+        else:
+            cprint("[!]-------------------------------------------[!]", 'red')
+            cprint("[!] You must Initialize the app first!", 'red', attrs=['bold'])
+            cprint("[.] Run the command: sshmanager init", 'yellow', attrs=['bold'])
+            cprint("[!]-------------------------------------------[!]", 'red')
+            sys.exit(1)
+
+    def ask_for_new_record(self):
+        # ASk for save the first ssh conection
+        answer = pyip.inputChoice(['ye  s', 'no', 'y','n'], "Do you want to create a new record with ssh data conection?(yes/no): ")
+        if answer in ['y', 'yes']:
+            #remove old database and re-run this method
+            self.new()
+
     def list(self):
+        self.check_init()
         query = "SELECT id, alias, user || '@' || host || ':' || port, updated_at  FROM SSH"
         conn = sqlite3.connect("ssh-manager.db")
         cursor = conn.cursor()
         cursor.execute(query)
         rows = cursor.fetchall()
-        print(tabulate(rows, headers=["id", "alias", "host" ,"last_update"]))
-
+        if len(rows) == 0:
+            cprint("[!] You dont have saved conections.", 'yellow')
+            self.ask_for_new_record()
+        else:
+            print(tabulate(rows, headers=["id", "alias", "host" ,"last_update"]))
         conn.close()
 
+    def export(self):
+        self.check_init()
+        # I need to add more args and export to sql or csv, and export path
+        con = sqlite3.connect('ssh-manager.db')
+        os.remove('dump.sql') if os.path.isfile('dump.sql') else logger.debug('No dump file created')
+        with open('dump.sql', 'w') as f:
+            for line in con.iterdump():
+                f.write('%s\n' % line)
+        cprint("[.] A file with dump was created on this folder.","green")
 
+    # def import(self):
+    #     print("Ismporting...")
 
 main()
